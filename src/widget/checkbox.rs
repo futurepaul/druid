a4171922a8b3446b97866258e0becfe02e61d3ae
Paul Miller <paul@pauljmiller.com>
2019-05-20 12:31:47 -0700

    save some checkbox work

--- src/widget/checkbox.rs
+++ src/widget/checkbox.rs
@@ -18,105 +18,104 @@ use std::any::Any;
 
 use crate::widget::Widget;
 use crate::{
-    BoxConstraints, Geometry, HandlerCtx, Id, LayoutCtx, LayoutResult, MouseEvent, PaintCtx, Ui,
+  BoxConstraints, Geometry, HandlerCtx, Id, LayoutCtx, LayoutResult, MouseEvent, PaintCtx, Ui,
 };
 
-use kurbo::{Line, Rect, BezPath};
+use kurbo::{BezPath, Rect};
 use piet::{FillRule, RenderContext};
 
 const BOX_HEIGHT: f64 = 19.;
 
+use svg_to_piet::*;
+
 pub struct Checkbox {
-    value: bool
+  value: bool,
 }
 
 impl Checkbox {
-    pub fn new(value: bool) -> Checkbox {
-        Checkbox {
-          value 
-        }
-    }
-    pub fn ui(self, ctx: &mut Ui) -> Id {
-        ctx.add(self, &[])
-    }
+  pub fn new(value: bool) -> Checkbox {
+    Checkbox { value }
+  }
+  pub fn ui(self, ctx: &mut Ui) -> Id {
+    ctx.add(self, &[])
+  }
 }
 
 impl Widget for Checkbox {
-    fn paint(&mut self, paint_ctx: &mut PaintCtx, geom: &Geometry) {
+  fn paint(&mut self, paint_ctx: &mut PaintCtx, geom: &Geometry) {
+    let background_color = 0x55_55_55_ff;
+    let foreground_color = 0xf0f0eaff;
 
-        let background_color = 0x55_55_55_ff;
-        let foreground_color = 0xf0f0eaff;
+    //Paint the background
+    let brush = paint_ctx.render_ctx.solid_brush(background_color).unwrap();
 
-        //Paint the background
-        let brush = paint_ctx.render_ctx.solid_brush(background_color).unwrap();
+    let (x, y) = geom.pos;
+    let (x, y) = (x as f64, y as f64);
+    let (width, height) = geom.size;
+    let rect = Rect::new(x, y, x + BOX_HEIGHT, y + BOX_HEIGHT);
 
-        let (x, y) = geom.pos;
-        let (x, y) = (x as f64, y as f64);
-        let (width, height) = geom.size;
-        let rect = Rect::new(
-            x,
-            y,
-            x + BOX_HEIGHT,
-            y + BOX_HEIGHT,
-        );
+    paint_ctx.render_ctx.fill(rect, &brush, FillRule::NonZero);
 
-        paint_ctx.render_ctx.fill(rect, &brush, FillRule::NonZero);
+    //Paint the check
+    let brush = paint_ctx.render_ctx.solid_brush(foreground_color).unwrap();
 
-        //Paint the check 
-        let brush = paint_ctx.render_ctx.solid_brush(foreground_color).unwrap();
+    let mut path = BezPath::new();
+    path.moveto((x + 3.0, y + 9.0));
+    path.lineto((x + 8.0, y + 14.0));
+    path.lineto((x + 15.0, y + 4.0));
 
-        let mut path = BezPath::new();
-        path.moveto((x + 3.0, y + 9.0));
-        path.lineto((x + 8.0, y + 14.0));
-        path.lineto((x + 15.0, y + 4.0));
+    paint_ctx.render_ctx.stroke(path, &brush, 2.0, None);
 
-        paint_ctx.render_ctx.stroke(path, &brush, 2.0, None);
+    //Cover the check if false
+    if (self.value == false) {
+      let brush = paint_ctx.render_ctx.solid_brush(background_color).unwrap();
 
-        //Cover the check if false
-        if (self.value == false) {
-            let brush = paint_ctx.render_ctx.solid_brush(background_color).unwrap();
+      let rect = Rect::new(x + 2., y + 2., x + BOX_HEIGHT - 2., y + BOX_HEIGHT - 2.);
 
-            let rect = Rect::new(
-                x + 2.,
-                y + 2.,
-                x + BOX_HEIGHT - 2.,
-                y + BOX_HEIGHT - 2.,
-            );
-
-            paint_ctx.render_ctx.fill(rect, &brush, FillRule::NonZero);
-        }
+      paint_ctx.render_ctx.fill(rect, &brush, FillRule::NonZero);
     }
 
-    fn layout(
-        &mut self,
-        bc: &BoxConstraints,
-        _children: &[Id],
-        _size: Option<(f32, f32)>,
-        _ctx: &mut LayoutCtx,
-    ) -> LayoutResult {
-        LayoutResult::Size(bc.constrain((BOX_HEIGHT as f32, BOX_HEIGHT as f32)))
-    }
+    #[derive(SvgToPiet)]
+    #[file = "face.svg"]
+    struct Face;
 
-    fn mouse(&mut self, event: &MouseEvent, ctx: &mut HandlerCtx) -> bool {
-        if event.count > 0 {
-            ctx.set_active(true);
-            self.value = !self.value;
-            ctx.send_event(self.value);
-        } else {
-            ctx.set_active(false);
-        }
-        ctx.invalidate();
-        true
-    }
+    #[derive(SvgToPiet)]
+    #[file = "tiger.svg"]
+    struct Tiger;
 
-    fn poke(&mut self, payload: &mut Any, ctx: &mut HandlerCtx) -> bool {
-        if let Some(value) = payload.downcast_ref::<bool>() {
-            self.value = *value;
-            ctx.invalidate();
-            true
-        } else {
-            println!("downcast failed");
-            false
-        }
+    Tiger::draw(paint_ctx, 0., 0.);
+  }
+
+  fn layout(
+    &mut self,
+    bc: &BoxConstraints,
+    _children: &[Id],
+    _size: Option<(f32, f32)>,
+    _ctx: &mut LayoutCtx,
+  ) -> LayoutResult {
+    LayoutResult::Size(bc.constrain((BOX_HEIGHT as f32, BOX_HEIGHT as f32)))
+  }
+
+  fn mouse(&mut self, event: &MouseEvent, ctx: &mut HandlerCtx) -> bool {
+    if event.count > 0 {
+      ctx.set_active(true);
+      self.value = !self.value;
+      ctx.send_event(self.value);
+    } else {
+      ctx.set_active(false);
     }
+    ctx.invalidate();
+    true
+  }
+
+  fn poke(&mut self, payload: &mut Any, ctx: &mut HandlerCtx) -> bool {
+    if let Some(value) = payload.downcast_ref::<bool>() {
+      self.value = *value;
+      ctx.invalidate();
+      true
+    } else {
+      println!("downcast failed");
+      false
+    }
+  }
 }
